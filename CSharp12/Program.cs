46ae36625f923d5c1de16076788274be56cea3a2
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-25 13:09:13 +0300

    Add project files.

---
+++ CSharp12/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Metodlar metodlar = new Metodlar();
+metodlar.Yaz("deneme1");
+metodlar.Yaz("deneme1", "deneme2");
+
+
+class Metodlar
+{
+    public void Yaz(string c1)
+    {
+        Console.WriteLine("Tek değişkenli metod çalıştı.." + c1);
+    }
+
+    public void Yaz(string c1, string c2)
+    {
+        Console.WriteLine("İki değişkenli metod çalıştı.." + c1 + " " + c2);
+    }
+}
